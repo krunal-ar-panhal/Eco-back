5b68120e43242a2e5c830d898c17c46545d70821
krunal-ar-panhal <krunalapnchalkp2123@gmail.com>
2024-10-02 18:39:18 +0530

    setup for cloudinary

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectCloudinary from "./config/cloudinary";
 
 dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 4000 ;
+const PORT = process.env.PORT || 4000;
+connectCloudinary();
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-mongoose.connect(process.env.MONGODB_URL)
-.then(()=>{
-    console.log('connected to mongodb')
-    
-}).catch((error)=>{
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((error) => {
     console.log(error);
-    
-})
+  });
 
-app.get('/',(req,res)=>{
-    res.send('hello ji')
-})
+app.get("/", (req, res) => {
+  res.send("hello ji");
+});
 
-app.listen(PORT,()=>{
-    console.log(`port connected on ${PORT}`);
-    
-})
-
+app.listen(PORT, () => {
+  console.log(`port connected on ${PORT}`);
+});
